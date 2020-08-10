a5e1c40509a5cd19504ed976b38f110d66284494
Taegyun Kim <tagun1202@gmail.com>
2020-08-10 16:43:09 +0900

    fix: util and node is splitted

--- main.py
+++ main.py
@@ -2,158 +2,32 @@ import math
 import pygame
 
 from algorithms import astar  # pylint: disable=E0401
+import util
 
 WIDTH = 800
 
 WINDOW = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("Path simulator")
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-YELLOW = (255, 255, 0)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-PURPLE = (128, 0, 128)
-ORANGE = (255, 165, 0)
-GREY = (128, 128, 128)
-BROWN = (210, 105, 30)
 
 # TODO: change update_neighbors parts with following
 # Additionally consider if path can go to diagonal
 # MOVEMENTS = [(1, 0), (-1, 0), (0, 1), (0,-1)]
 
 
-class Node:
-    def __init__(self, row, col, width, total_rows):
-        self.row = row
-        self.col = col
-        self.width = width
-        self.x = row * width
-        self.y = col * width
-        self.color = WHITE
-        self.neighbors = []
-        self.total_rows = total_rows
-
-    def get_pos(self):
-        return self.col, self.row
-
-    def is_closed(self):
-        return self.color == RED
-
-    def is_open(self):
-        return self.color == GREEN
-
-    def is_barrier(self):
-        return self.color == BLACK
-
-    def is_start(self):
-        return self.color == ORANGE
-
-    def is_end(self):
-        return self.color == PURPLE
-
-    def reset(self):
-        self.color = WHITE
-
-    def make_closed(self):
-        self.color = RED
-
-    def make_open(self):
-        self.color = GREEN
-
-    def make_barrier(self):
-        self.color = BLACK
-
-    def make_start(self):
-        self.color = ORANGE
-
-    def make_end(self):
-        self.color = PURPLE
-
-    def make_path(self):
-        self.color = BROWN
-
-    def draw(self, window):
-        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.width))
-
-    def update_neighbors(self, grid):
-        self.neighbors = []
-        # check bottom
-        if (
-            self.row < self.total_rows - 1
-            and not grid[self.row + 1][self.col].is_barrier()
-        ):
-            self.neighbors.append(grid[self.row + 1][self.col])
-        # check above
-        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():
-            self.neighbors.append(grid[self.row - 1][self.col])
-        # check left
-        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():
-            self.neighbors.append(grid[self.row][self.col - 1])
-        # check right
-        if (
-            self.col < self.total_rows - 1
-            and not grid[self.row][self.col + 1].is_barrier()
-        ):
-            self.neighbors.append(grid[self.row][self.col + 1])
-
-    def __lt__(self, other):
-        return False
-
-
-# TODO: split (util) ####################################################################
-def make_grid(rows, width):
-    grid = []
-    gap = width // rows
-    for i in range(rows):
-        grid.append([])
-        for j in range(rows):
-            node = Node(i, j, gap, rows)
-            grid[i].append(node)
-    return grid
-
-
-def draw_grid(window, rows, width):
-    gap = width // rows
-    for i in range(rows):
-        pygame.draw.line(window, GREY, (0, i * gap), (width, i * gap))
-        pygame.draw.line(window, GREY, (i * gap, 0), (i * gap, width))
-
-
-def draw(window, grid, rows, width):
-    for row in grid:
-        for node in row:
-            node.draw(window)
-
-    draw_grid(window, rows, width)
-    pygame.display.update()
-
-
-def get_clicked_pos(pos, rows, width):
-    gap = width // rows
-    y, x = pos
-
-    row = y // gap
-    col = x // gap
-    return row, col
-
-
-##################################################################################################
-
-
 def main(window, width):
     ROWS = 50
-    grid = make_grid(ROWS, width)
+    grid = util.make_grid(ROWS, width)
 
     start = None
     end = None
-    # TODO: implement found
+    # TODO: handle when path is not found
     found = False
 
     is_running = True
 
     while is_running:
-        draw(window, grid, ROWS, width)
+        util.draw(window, grid, ROWS, width)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # pylint: disable=E1101
@@ -161,7 +35,7 @@ def main(window, width):
 
             if pygame.mouse.get_pressed()[0]:  # clicking left mouse button
                 pos = pygame.mouse.get_pos()
-                row, col = get_clicked_pos(pos, ROWS, width)
+                row, col = util.get_clicked_pos(pos, ROWS, width)
                 node = grid[row][col]
 
                 # first click = start
@@ -178,7 +52,7 @@ def main(window, width):
 
             elif pygame.mouse.get_pressed()[2]:  #  click right mouse button
                 pos = pygame.mouse.get_pos()
-                row, col = get_clicked_pos(pos, ROWS, width)
+                row, col = util.get_clicked_pos(pos, ROWS, width)
                 node = grid[row][col]
 
                 if node.is_start():
@@ -195,7 +69,7 @@ def main(window, width):
                             node.update_neighbors(grid)
                     algorithm = astar.A_star()
                     found = algorithm.search(
-                        lambda: draw(window, grid, ROWS, width), grid, start, end
+                        lambda: util.draw(window, grid, ROWS, width), grid, start, end
                     )
 
     pygame.quit()  # pylint: disable=E1101
---
+++ util.py
@@ -0,0 +1,41 @@
+import pygame
+
+# TODO: GREY should not be in node
+from node import Node, GREY
+
+
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node(i, j, gap, rows)
+            grid[i].append(node)
+    return grid
+
+
+def draw_grid(window, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(window, GREY, (0, i * gap), (width, i * gap))
+        pygame.draw.line(window, GREY, (i * gap, 0), (i * gap, width))
+
+
+def draw(window, grid, rows, width):
+    for row in grid:
+        for node in row:
+            node.draw(window)
+
+    draw_grid(window, rows, width)
+    pygame.display.update()
+
+
+def get_clicked_pos(pos, rows, width):
+    gap = width // rows
+    y, x = pos
+
+    row = y // gap
+    col = x // gap
+    return row, col
+
