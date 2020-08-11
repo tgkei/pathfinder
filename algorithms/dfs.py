7309756910d97d9e6705d1775d2b5a0f7117a0f2
Taegyun Kim <tagun1202@gmail.com>
2020-08-11 19:01:09 +0900

    feat: dfs is added

---
+++ algorithms/dfs.py
@@ -0,0 +1,38 @@
+from collections import deque
+from algorithms.abcalgo import Abc_algo  # pylint: disable=E0401
+import pygame
+
+
+class DFS(Abc_algo):
+    def search(self, draw, grid, start, end):
+        open_queue = deque()
+        open_queue.append(start)
+        open_set = {start}  # prevent to cycle the path
+        came_from = dict()
+
+        while open_queue:
+            for event in pygame.event.get():  # pylint: disable=E1101
+                if event.type == pygame.QUIT:  # pylint: disable=E1101
+                    pygame.quit()  # pylint: disable=E1101
+
+            current = open_queue.pop()
+
+            if current == end:
+                self.find_path(came_from, current, draw)
+                end.make_end()
+                start.make_start()
+                return True
+
+            for neighbor in current.neighbors:
+                if neighbor not in open_set:
+                    came_from[neighbor] = current
+                    open_queue.append(neighbor)
+                    open_set.add(neighbor)
+                    neighbor.make_open()
+
+            if current != start:
+                current.make_closed()
+
+            draw()
+
+        return False
